042e9823fcf34db69c547370c74730c8dfd3a819
Ildi Dvorani <dvoranii@ildidev.com>
2024-03-06 10:47:08 -0500

    added cors

--- server.js
+++ server.js
@@ -1,9 +1,11 @@
 import express from "express";
 import { saveContact } from "./src/utils/firebaseAdmin.js";
+import cors from "cors";
 
 const app = express();
 const port = process.env.PORT || 3000;
 
+app.use(cors());
 app.use(express.json());
 
 app.get("/contacts", (req, res) => {
